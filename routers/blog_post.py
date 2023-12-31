556e79a593feed334eb1513a3303c492cc600c85
Ndifoinhilar@gmail.com <ndifoinhilary@gmail.com>
2023-12-31 22:38:48 +0100

    Added metadata

--- routers/blog_post.py
+++ routers/blog_post.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query, Path, Body
 from pydantic import BaseModel
 from typing import Optional
 
@@ -14,3 +14,14 @@ class BlogModel(BaseModel):
 @router.post("/create/{id}")
 def create_blog(blog: BlogModel, id: int, version: int = 1):
     return {"ID": id, "Data": blog, "version": version}
+
+
+@router.post("/new/{id}/comment")
+def create_comment(
+    blog: BlogModel,
+    id: int,
+    comment_id: int = Query(
+        None, title="Id of the comment", description="some description", deprecated=True
+    ),
+):
+    return {"blog": blog, "id": id, "comment_id": comment_id}
