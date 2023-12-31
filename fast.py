97821a25b2b47325a5adf3804648314d7807fe59
Ndifoinhilar@gmail.com <ndifoinhilary@gmail.com>
2023-12-31 20:24:37 +0100

    Working with fastapi and added routers to structered the code

--- fast.py
+++ fast.py
@@ -1,32 +1,12 @@
+from typing import Optional
 from fastapi import FastAPI, status, Response
-
+from routers import blog_get
 
 app = FastAPI()
 
+app.include_router(blog_get.router)
 
-@app.get('/')
+
+@app.get("/")
 def hello():
-    return {'Messages' : 'Testing this shirt stuff'}
-
-# working now with status code
-
-
-@app.get("/blog/{id}", status_code=status.HTTP_404_NOT_FOUND)
-def get_blog(id: int, response: Response):
-    if id > 5:
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return {"Error": f'Blog {id} not found'}
-    else:
-        response.status_code = status.HTTP_200_OK
-        return {"Message": f"Blog with {id}"}
-
-# how to add tags,  summary and description in fastapi 
-@app.get(
-    "/blog/all",
-    tags=["blog"],
-    summary="Retrieve all blogs",
-    description="to get all the blog in the db",
-)
-def get_all_blog():
-    return {"Message": "how are you doing "}
-
+    return {"Messages": "Testing this shirt stuff"}
---
+++ routers/blog_get.py
@@ -0,0 +1,32 @@
+from typing import Optional
+from fastapi import FastAPI, status, Response, APIRouter
+
+
+router = APIRouter(prefix="/blog", tags=["blog"])
+
+
+# working now with status code
+
+
+router.get("/{id}", status_code=status.HTTP_404_NOT_FOUND)
+
+
+def get_blog(id: int, response: Response):
+    if id > 5:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"Error": f"Blog {id} not found"}
+    else:
+        response.status_code = status.HTTP_200_OK
+        return {"Message": f"Blog with {id}"}
+
+
+# how to add tags,  summary and description in fastapi
+router.get(
+    "/all",
+    summary="Retrieve all blogs",
+    description="to get all the blog in the db",
+)
+
+
+def get_all_blog(page=1, page_size: Optional[int] = None):
+    return {"Message": f"{page} in the blog of size {page_size}"}
