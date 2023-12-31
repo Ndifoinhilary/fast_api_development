cd7a1ea52a92b4aca0b341c3ca2e574b34403d8e
Ndifoinhilar@gmail.com <ndifoinhilary@gmail.com>
2023-12-31 21:55:59 +0100

    Created a model and tested it

--- routers/blog_post.py
+++ routers/blog_post.py
@@ -1,9 +1,16 @@
 from fastapi import APIRouter
-
+from pydantic import BaseModel
+from typing import Optional
 
 router = APIRouter(prefix="/blog/post", tags=["blog"])
 
 
+class BlogModel(BaseModel):
+    title: str
+    content: str
+    published: Optional[bool]
+
+
 @router.post("/create")
-def create_blog():
-    pass
+def create_blog(blog: BlogModel):
+    return {"Data": blog}
