5cae2adbd7e18c9c4d10b44e1adcdb19298a7844
Ndifoinhilar@gmail.com <ndifoinhilary@gmail.com>
2024-01-11 07:55:28 +0100

    perform CRUD operation

--- db/db_user.py
+++ db/db_user.py
@@ -16,3 +16,39 @@ def create_user(db: Session, request: UserBase):
     db.refresh(new_user)
 
     return new_user
+
+
+# to retrieve all users of the systems
+def get_all_user(db: Session):
+    return db.query(DbUser).all()
+
+
+# getting s single user
+def get_single_user(db: Session, id: int):
+    return db.query(DbUser).filter(DbUser.id == id).first()
+
+
+# update a user in the db
+
+
+def update_user(db: Session, id: int, request: UserBase):
+    user = db.query(DbUser).filter(DbUser.id == id).first()
+    user.update(
+        {
+            DbUser.username: request.username,
+            DbUser.email: request.email,
+            DbUser.password: Hash.bcrypt(request.password),
+        }
+    )
+    db.commit()
+    return "OK"
+
+
+# delete a user
+
+
+def delete_user(db: Session, id: int):
+    user = db.query(DbUser).filter(DbUser.id == id).first()
+    db.delete(user)
+    db.commit()
+    return "Ok"
--- routers/user.py
+++ routers/user.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from db.database import get_db
@@ -6,7 +7,37 @@ from db import db_user
 
 router = APIRouter(prefix="/user", tags=["user"])
 
+# creating a user
+
 
 @router.post("/", response_model=UserDisplay)
 def create_user(request: UserBase, db: Session = Depends(get_db)):
     return db_user.create_user(db, request)
+
+
+# read users that's all users
+@router.get("/", response_model=List[UserDisplay])
+def list_all_users(db: Session = Depends(get_db)):
+    return db_user.get_all_user(db)
+
+
+# getting a single user from the db
+@router.get("/{id}", response_model=UserDisplay)
+def get_single_user(db: Session = Depends(get_db)):
+    return db_user.get_single_user(db, id)
+
+
+# updating a user in the db
+
+
+@router.post("/{id}/update")
+def update_user(request: UserBase, id: int, db: Session = Depends(get_db)):
+    return db_user.update_user(db, id, request)
+
+
+# deleting a user from the db
+
+
+@router.get("/{id}/delete")
+def delete_user(id: int, db: Session = Depends(get_db)):
+    return db_user.delete_user(db, id)
