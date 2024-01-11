2b4fea38dc4d3bbc70250b08e52f174b4c8ab913
Ndifoinhilar@gmail.com <ndifoinhilary@gmail.com>
2024-01-11 05:26:15 +0100

    Created a user and hash the password

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from routers import blog_post, blog_get
+from routers import blog_post, blog_get, user
 from db import models
 from db.database import engine
 
 app = FastAPI()
-
+app.include_router(user.router)
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
 
